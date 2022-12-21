03dd3e241df5adc63d8d3c058658b72f89593d7b
Bruce Lee <brucehlee@yahoo.ca>
2022-12-20 16:23:30 -0800

    Added Packing List

---
+++ x-marks-the-perfect-shot.js
@@ -0,0 +1,16 @@
+const moves = ['north', 'north', 'west', 'west', 'north', 'east','north']; 
+const finalPosition = function (moves) {
+  // Your code in here ...
+for(let move of moves){
+
+  if( move === 'north'){ 
+    
+    return move[i].length += 1;
+   }
+  
+}
+  
+  
+};
+
+finalPosition(north);
