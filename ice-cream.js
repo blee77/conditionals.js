8f9b83d1a0487bbf3ad8b6e3b74c0843deee2b5a
Bruce Lee <brucehlee@yahoo.ca>
2022-12-20 16:26:14 -0800

    Added Ice Cream

--- ice-cream.js
+++ ice-cream.js
@@ -1,4 +1,17 @@
-const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+let iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+// Your code below here...
+
+console.log(iceCreamFlavours);
+console.log("chocolate");
+
+
 iceCreamFlavours.push("root beer");
+
+console.log(iceCreamFlavours[5]);
+
+
+
+
 console.log(iceCreamFlavours.length);
- 
